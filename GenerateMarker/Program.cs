d7384e17c93355895df193cf6266169eae27ab61
Nelson Segarra <junicus@outlook.com>
2017-11-27 19:25:49 -0400

    Added tool to mark folders as generated so we don't get duplicates when first installed

---
+++ GenerateMarker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateMarker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var iberdir = Environment.GetEnvironmentVariable("IBERDIR");
+      var datedFolders = Directory.GetDirectories(iberdir, "20??????");
+      foreach(var datedFolder in datedFolders)
+      {
+        var markerFilePath = Path.Combine(datedFolder, "IRSIPAYGEN");
+        if(!File.Exists(markerFilePath))
+        {
+          using (var filestream = File.Create(markerFilePath)) { };
+        }
+      }
+    }
+  }
+}
